6302c4034a2bd01115dc379b55ae4a55107a7808
李龙 <lilong2576@hanweb.com>
2025-04-24 14:57:44 +0800

    feat: 搭建服务器端

--- express-server/routes/users.js
+++ express-server/routes/users.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/list',function(req,res,next){
+  res.json({
+    code: 200,
+    msg: 'success',
+    data: [
+      {
+        name: '张三',
+        age: 20
+      },
+      {
+        name: '李四',
+        age: 22
+      }
+    ]
+  });
+})
+
+router.post('/add',function(req,res,next){
+  
+  console.log(req.body)
+  res.json({
+    code: 200,
+    msg: 'success',
+    data: JSON.stringify(req.body) // 转成J
+  });
+})
 module.exports = router;
