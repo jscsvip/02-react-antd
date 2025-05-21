14454e06e87aec1ac7d80aa06ba2f622bb533352
李龙 <lilong2576@hanweb.com>
2025-05-21 11:53:46 +0800

    feat: 定义资讯分类的表格

--- antd-manager/src/pages/articles/categories.tsx
+++ antd-manager/src/pages/articles/categories.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
-import { PageContainer } from '@ant-design/pro-components';
+import { PageContainer, ProTable, ProColumns } from '@ant-design/pro-components';
 
+type DataType = {
+    id: string,
+    name: string,
+    dec: string
+}
 
 function ArticleCategories() {
-    return <PageContainer>ArticleCategories</PageContainer>
+    const column: ProColumns<DataType>[] = [
+        {
+            title: '序号',
+            dataIndex: 'id',
+            width: 200,
+        },
+        {
+            title: '名字',
+            dataIndex: 'name',
+        },
+        {
+            title: '简介',
+            dataIndex: 'dec',
+        },
+        {
+            title: '图片',
+            dataIndex: 'dec',
+        },
+        {
+            title: '操作',
+            hideInSearch: true,
+        },
+    ]
+    return <PageContainer>
+        <ProTable
+            rowKey="id"
+            columns={column}></ProTable>
+    </PageContainer>
 }
 
 export default ArticleCategories;
