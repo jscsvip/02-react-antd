4142af80e06ac9557ea641cdf963caf626ee22b7
李龙 <lilong2576@hanweb.com>
2025-05-27 11:56:16 +0800

    feat: 复用资讯分类的表格实现资讯列表

--- antd-manager/src/pages/articles/list.tsx
+++ antd-manager/src/pages/articles/list.tsx
@@ -1,9 +1,231 @@
-import React from 'react';
-import { PageContainer } from '@ant-design/pro-components';
+import React, { useEffect, useRef, useState } from 'react';
+import { PageContainer, ProTable, ProColumns,ModalForm,ProForm,ProFormText, ActionType, ProFormTextArea, ProFormItem } from '@ant-design/pro-components';
+import { loadDataAPI, addModelAPI, delByIdAPI, editModelAPI, delManyByIdsAPI } from '@/services/articles';
+import { Button, message, Popconfirm, Space, Image } from 'antd';
+import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
+import MyUpload from '@/components/my-upload';
+import { dalImg } from '@/utils/tools';
 
+type DataType = {
+    id: string;
+    title: string;
+    dec: string;
+    coverImage: string;
+}
 
 function ArticleList() {
-    return <PageContainer>ArticleList</PageContainer>
+    const actionRef = useRef<ActionType>(null); //用来绑定表单的内置事件
+    const [myForm] = ProForm.useForm(); // 获取当前表单实例
+    const [currentId, setCurrentId] = useState('');   // 当前编辑的id,判断是新增还是编辑
+    const [isShowEdit, setIsShowEdit] = useState(false);
+    const [selectIds, setSelectIds] = useState<string>(''); // 选中的id
+    const [imageUrl, setImageUrl] = useState<string>('');
+    useEffect(() => {
+        // 关闭弹窗时重置表单
+        if (!isShowEdit) {
+            myForm.resetFields(); // 重置表单
+            setCurrentId('');
+            setImageUrl('');
+        }
+    }, [isShowEdit])
+    const column: ProColumns<DataType>[] = [
+        {
+            title: '序号',
+            width: 80,
+            hideInSearch: true,
+            /*
+                自定义渲染
+                第一个参数：当前列的值
+                第二个参数：当前行的数据
+                第三个参数：当前行的索引                
+            */
+            renderText: (text, record, index, action) => {
+                // 根据当前页码和每页条数计算实际序号
+                const pageInfo = action?.pageInfo;
+                if (pageInfo) {
+                    const { current, pageSize } = pageInfo;
+                    return (current - 1) * pageSize + index + 1;
+                }
+                return index + 1;
+            }
+        },
+        {
+            title: '标题',
+            dataIndex: 'title',
+        },
+        {
+            title: '简介',
+            dataIndex: 'desc',
+            // hideInSearch: true,
+        },
+        // {
+        //     title: '内容',
+        //     dataIndex: 'content',
+        //     hideInSearch: true,
+        // },
+        {
+            title: '图片',
+            dataIndex: 'coverImage',
+            hideInSearch: true,
+            render: (text) => {
+                console.log(text);
+                if (text==='-') {
+                    return <span>{text}</span>
+                }else{
+                    return <Image src={dalImg(text as string)} alt="" width={100} height={100} />
+                }
+            }
+        },
+        {
+            title: '操作',
+            hideInSearch: true,
+            render: (text, record) => {
+                return <Space>
+                    <Button type="primary" icon={<EditOutlined/>} size='small' onClick={
+                        () => {
+                            setIsShowEdit(true);
+                            setCurrentId(record.id);
+                            setImageUrl(record.coverImage);
+                            myForm.setFieldsValue(record); // 给表单赋值,回显数据
+                        }
+                    }>编辑</Button>
+                    <Popconfirm
+                        title="确认删除?"
+                        onConfirm={async () => {
+                            console.log(record);
+                            await delByIdAPI(record.id).then((res) => {
+                                // console.log(res);
+                                message.success('删除成功');
+                                // 刷新表格
+                                actionRef.current?.reload();
+                            })
+                        }}
+                    >
+                        <Button type="primary" icon={<DeleteOutlined/>} size='small' danger>删除</Button>
+                    </Popconfirm>
+                </Space>
+            }
+        },
+    ]
+    return <PageContainer>
+        <ModalForm title="编辑" 
+            form={myForm}
+            open={isShowEdit}
+            onOpenChange={setIsShowEdit}
+            modalProps={{
+                maskClosable: false,
+                destroyOnClose: true,
+                width: 600,
+            }}
+            onFinish={
+                async (values) => {
+                    // console.log(values);
+                    if(currentId) {
+                        await editModelAPI(currentId, {...values,coverImage:imageUrl})
+                        message.success('编辑成功');
+                    }else{
+                        await addModelAPI( {...values,coverImage:imageUrl})
+                        message.success('添加成功');
+                    }
+                    setIsShowEdit(false);
+                        // message.success('添加成功');
+                        // 刷新表格
+                    actionRef.current?.reload();
+                    
+                }
+            }
+        >
+            {/* <ProForm
+                labelCol={{ span: 4 }}
+                onFinish={
+                    (values) => {
+                        console.log(values);
+                    }
+                }
+            > */}
+                <ProFormText name="title" label="标题" rules={
+                    [
+                        {
+                            required: true,
+                            message: '请输入标题',
+                        }
+                    ]
+                }/>
+                <ProFormItem label="图片">
+                    <MyUpload
+                        imageUrl={imageUrl}
+                        setImageUrl={setImageUrl}
+                    ></MyUpload>
+                </ProFormItem>
+                <ProFormTextArea name="desc" label="简介" />
+                <ProFormTextArea name="content" label="内容" />
+                {/* <ProFormText name="img" label="图片" /> */}
+            {/* </ProForm> */}
+        </ModalForm>
+        <ProTable
+            actionRef={actionRef}
+            rowKey="id"
+            columns={column}
+            request={loadDataAPI}
+            pagination={{
+                pageSize: 10
+            }}
+            rowSelection={{
+                type: 'checkbox',
+                onChange: (selecteRowKeys) => {
+                    setSelectIds(selecteRowKeys.join(','));
+                    console.log(selecteRowKeys); // 选中的id
+                },
+            }}
+            headerTitle={
+                <>
+                <Space>
+                <Button 
+                    type="primary" 
+                    icon={<PlusOutlined/>} 
+                    size='small' 
+                    onClick={
+                        () => {
+                            setIsShowEdit(true);
+                        }
+                    }>新增</Button>
+                    <Popconfirm  title="确认删除?"
+                        onConfirm={
+                            async () => {
+                                await delManyByIdsAPI(selectIds).then((res) => {
+                                    // console.log(res);
+                                    message.success('删除成功');
+                                    // 刷新表格
+                                    actionRef.current?.reload();
+                                })
+                            }
+                        }>
+                        <Button 
+                        type="primary" 
+                        icon={<DeleteOutlined/>} 
+                        size='small' 
+                        danger 
+                        style={{display:selectIds===''?'none':''}}
+                        >批量删除</Button>
+                    </Popconfirm>
+                
+                </Space>
+                    
+                    
+                </>
+                
+            }
+            options={{
+                setting: {
+                    listsHeight: 400,
+                },
+                density: true,
+                fullScreen: true,
+                reload: true,
+            }}
+            dateFormatter="string"
+        ></ProTable>
+    </PageContainer>
 }
 
 export default ArticleList;
---
+++ antd-manager/src/services/articles.ts
@@ -0,0 +1,114 @@
+import { request } from '@umijs/max';
+/**
+ * 查询列表
+ * @param params 
+ * @returns 
+ */
+export const loadDataAPI = async (
+    // 第一个参数 params 查询表单和 params 参数的结合
+    // 第一个参数中一定会有 pageSize 和  current ，这两个参数是 antd 的规范
+    params: any & {
+      pageSize: number;
+      current: number;
+    },
+    // sort,
+    // filter,
+  ) => {
+    const res = await request('/api/v1/admin/articles', {
+      method: 'GET',
+      params: {
+        page:params.current,
+        pageSize:params.pageSize,
+        ...params,
+      },
+    });
+    // 这里需要返回一个 Promise,在返回之前你可以进行数据转化
+    // 如果需要转化参数可以在这里进行修改
+    // const msg = await myQuery({
+    //   page: params.current,
+    //   pageSize: params.pageSize,
+    // });
+    // console.log(res);
+    return {
+      ...res.data,
+      // success 请返回 true，
+      // 不然 table 会停止解析数据，即使有数据
+      success: true,
+      // 不传会使用 data 的长度，如果是分页一定要传
+    //   total: 21,
+    };
+  }
+/*
+* 新增
+* @param data
+* @returns
+*/
+  export const addModelAPI = async (
+    data: any,
+  ) => {
+    const res = await request('/api/v1/admin/articles', {
+      method: 'POST',
+      data: {
+        ...data,
+      },
+    });
+    return res;
+  }
+/**
+ * 修改
+ * @param id 
+ * @param data 
+ * @returns 
+ */
+  export const editModelAPI = async (
+    id:string,
+    data: any,
+  ) => {
+    const res = await request(`/api/v1/admin/articles/${id}`, {
+      method: 'PUT',
+      data: {
+       ...data,
+      },
+    });
+    return res;
+  }
+
+  /***
+   * 根据id获取单条记录
+   * @param id
+   * @returns
+   */
+export const loadDataByIdAPI = async (
+  id: string,
+) => {
+  const res = await request(`/api/v1/admin/articles/${id}`, {
+    method: 'GET',
+  });
+  return res;
+}
+/**
+ * 根据id删除一个
+ * @param id 
+ * @returns 
+ */
+export const delByIdAPI = async (
+  id: string,
+) => {
+  const res = await request(`/api/v1/admin/articles/${id}`, {
+    method: 'DELETE',
+  });
+  return res;
+}
+/**
+ * 批量删除,多个id用逗号隔开
+ * @param ids 
+ * @returns 
+ */
+export const delManyByIdsAPI = async (
+  ids: string,
+) => {
+  const res = await request(`/api/v1/admin/articles/delete_many?ids=${ids}`, {
+    method: 'DELETE',
+  });
+  return res;
+}
